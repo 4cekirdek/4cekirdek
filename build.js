c6890c675b73d7e2a6c53a89186150b3d3b310b4
Samet Çetin <46278098+imsamet@users.noreply.github.com>
2025-01-07 01:03:57 +0300

    added table style, build fix

--- build.js
+++ build.js
@@ -2,16 +2,53 @@ const sass = require("sass");
 const fs = require("fs");
 const path = require("path");
 
-const inputFile = path.join(__dirname, "src/index.scss");
+const inputIndexFile = path.join(__dirname, "src/index.scss");
+const inputFonts = path.join(__dirname, "src/styles/_fonts.scss");
+const inputTypography = path.join(__dirname, "src/styles/_typography.scss");
+const inputUi = path.join(__dirname, "src/styles/_ui.scss");
+const inputBorder = path.join(__dirname, "src/styles/_border.scss");
+const inputSpacing = path.join(__dirname, "src/styles/_spacing.scss");
+const inputSizing = path.join(__dirname, "src/styles/_sizing.scss");
+const inputGridSystem = path.join(__dirname, "src/styles/_grid-system.scss");
+const inputEffect = path.join(__dirname, "src/styles/_effect.scss");
+const inputTable = path.join(__dirname, "src/styles/_table.scss");
 
-const outputFile = path.join(__dirname, "style.css");
+const outputIndexFile = path.join(__dirname, "dist/index.css");
+const outputFonts = path.join(__dirname, "dist/fonts.css");
+const outputTypography = path.join(__dirname, "dist/typography.css");
+const outputUi = path.join(__dirname, "dist/ui.css");
+const outputBorder = path.join(__dirname, "dist/border.css");
+const outputSpacing = path.join(__dirname, "dist/spacing.css");
+const outputSizing = path.join(__dirname, "dist/sizing.css");
+const outputGridSystem = path.join(__dirname, "dist/grid-system.css");
+const outputEffect = path.join(__dirname, "dist/effect.css");
+const outputTable = path.join(__dirname, "dist/table.css");
 
 function compileScss() {
   try {
-    const result = sass.compile(inputFile);
+    const resultIndexFile = sass.compile(inputIndexFile);
+    const resultFonts = sass.compile(inputFonts);
+    const resultTypography = sass.compile(inputTypography);
+    const resultUi = sass.compile(inputUi);
+    const resultBorder = sass.compile(inputBorder);
+    const resultSpacing = sass.compile(inputSpacing);
+    const resultSizing = sass.compile(inputSizing);
+    const resultGridSystem = sass.compile(inputGridSystem);
+    const resultEffect = sass.compile(inputEffect);
+    const resultTable = sass.compile(inputTable);
 
-    fs.writeFileSync(outputFile, result.css);
-    console.log(`SCSS başarıyla ${outputFile} dosyasına dönüştürüldü!`);
+    fs.writeFileSync(outputIndexFile, resultIndexFile.css);
+    fs.writeFileSync(outputFonts, resultFonts.css);
+    fs.writeFileSync(outputTypography, resultTypography.css);
+    fs.writeFileSync(outputUi, resultUi.css);
+    fs.writeFileSync(outputBorder, resultBorder.css);
+    fs.writeFileSync(outputSpacing, resultSpacing.css);
+    fs.writeFileSync(outputSizing, resultSizing.css);
+    fs.writeFileSync(outputGridSystem, resultGridSystem.css);
+    fs.writeFileSync(outputEffect, resultEffect.css);
+    fs.writeFileSync(outputTable, resultTable.css);
+
+    console.log(`SCSS başarıyla ${outputIndexFile} dosyasına dönüştürüldü!`);
   } catch (err) {
     console.error("Hata oluştu:", err.message);
   }
